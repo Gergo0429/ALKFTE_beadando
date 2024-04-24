529861474f311131585335b24b2d71dc9cc35f1d
Gergo0429 <zsigmondgergo@gmail.com>
2024-04-24 17:11:44 +0300

    Projekt generálva

---
+++ src/main/java/edu/gde/alkfte_futok/AlkfteFutokApplication.java
@@ -0,0 +1,13 @@
+package edu.gde.alkfte_futok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkfteFutokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkfteFutokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/gde/alkfte_futok/AlkfteFutokApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gde.alkfte_futok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkfteFutokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
