32e8a79eba9dab90f95dbec62740777af5995dd7
Gergo0429 <zsigmondgergo@gmail.com>
2024-04-25 16:03:02 +0300

    Repository-k implementálva

---
+++ src/main/java/edu/gde/alkfte_futok/repositories/EredmenyRepo.java
@@ -0,0 +1,7 @@
+package edu.gde.alkfte_futok.repositories;
+
+import edu.gde.alkfte_futok.models.Eredmeny;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EredmenyRepo extends JpaRepository<Eredmeny, Long> {
+}
---
+++ src/main/java/edu/gde/alkfte_futok/repositories/VersenyRepo.java
@@ -0,0 +1,7 @@
+package edu.gde.alkfte_futok.repositories;
+
+import edu.gde.alkfte_futok.models.Verseny;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VersenyRepo extends JpaRepository<Verseny, Long> {
+}
